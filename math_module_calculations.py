7c55045b05f176cf8dea1532eef15a76c0008c91
Devesh Kumar <deyvesshkumar@gmail.com>
2025-04-22 20:38:40 +0530

    adding math_module_calculations.py

---
+++ math_module_calculations.py
@@ -0,0 +1,12 @@
+
+from math import *
+
+number = int(input('Enter a number: '))
+
+square_root = sqrt(number)
+logarithm = log(number)
+sine = sin(number)
+
+print(f'Square root: {square_root}')
+print(f'Logarithm: {logarithm}')
+print(f'Sine: {sine}')
